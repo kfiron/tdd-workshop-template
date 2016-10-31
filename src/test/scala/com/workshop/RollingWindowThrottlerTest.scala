ff05a266fd0a50a0346c6fa228f2059a34edb77a
kfirb <kfirb@wix.com>
2016-10-31 06:42:38 +0200

    j-second-test

--- src/test/scala/com/workshop/RollingWindowThrottlerTest.scala
+++ src/test/scala/com/workshop/RollingWindowThrottlerTest.scala
@@ -10,12 +10,30 @@ class RollingWindowThrottlerTest extends SpecificationWithJUnit{
       val throttler = new RollingWindowThrottler(max = 1, durationWindow = 1.minute) 
       throttler.tryAcquire("192.168.2.1") must beTrue
     }
+    "Throttle second request" in {
+      val throttler = new RollingWindowThrottler(max = 1, durationWindow = 1.minute)
+      throttler.tryAcquire("192.168.2.1") must beTrue
+      throttler.tryAcquire("192.168.2.1") must beFalse
+    }
     
   }
 
 }
 
 class RollingWindowThrottler(max: Int, durationWindow: FiniteDuration) {
-  def tryAcquire(key: String): Boolean = true
   
+  val counter = Counter()
+  
+  def tryAcquire(key: String): Boolean = {
+    val invocationCount = counter.incrementAndGet
+    invocationCount <= max
+  }
+  
+}
+
+case class Counter(var count: Int = 0){
+  def incrementAndGet: Int = {
+    count = count + 1
+    count    
+  }  
 }
